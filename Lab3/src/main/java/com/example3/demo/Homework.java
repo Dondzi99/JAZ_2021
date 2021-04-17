cca398ca5881b97eeea870dc130be0bf25e14802
Paweł Dondziak <56679544+Dondzi99@users.noreply.github.com>
2021-04-17 19:09:05 +0200

    commiting homework Lab3

---
+++ Lab3/src/main/java/com/example3/demo/Homework.java
@@ -0,0 +1,38 @@
+package com.example3.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/homework")
+public class Homework {
+
+    @GetMapping("/getEntity")
+    ResponseEntity<String> getEntity(@RequestParam String reqParam) {
+        return ResponseEntity.ok(reqParam);
+    }
+    @GetMapping("/getEntity/{MojString}")
+    ResponseEntity<String> getEntityWithPathV(@PathVariable String MojString) {
+        return ResponseEntity.ok(MojString);
+    }
+
+    @PutMapping("/putEntity")
+    ResponseEntity<String> putEntity(@RequestBody String MojString) {
+        return ResponseEntity.ok(MojString);
+    }
+
+    @PutMapping("/putEntity/{MojString}")
+    ResponseEntity<String> putEntityWithPathV(@PathVariable String MojString) {
+        return ResponseEntity.ok(MojString);
+    }
+
+    @PostMapping("/postEntity")
+    ResponseEntity<String> postEntity(@RequestBody String MojString) {
+        return ResponseEntity.ok(MojString);
+    }
+
+    @DeleteMapping("/deleteEntity/{MojString}")
+    ResponseEntity<String> deleteEntity(@PathVariable String MojString){
+        return ResponseEntity.ok().build();
+    }
+}
