003d55a19008b330b9307aca461a4d8876254731
Paweł Dondziak <56679544+Dondzi99@users.noreply.github.com>
2021-07-03 18:14:42 +0200

    last add

--- jaz-s18946-nbp/src/main/java/pj/s18946/demo/controller.java
+++ jaz-s18946-nbp/src/main/java/pj/s18946/demo/controller.java
@@ -23,15 +23,12 @@ public class controller {
     // endpoint pozwalający na otrzymanie informacji o sredniej watrosci waluty
     // z wczesniejszym przekazaniem przez uzytkownika nazwy waluty, daty od, daty do(podanych w dniach)
     // w odpowiedzi otrzymujemy obikt json typu currency
-    String pattern = "yyyy-MM-dd";
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
     @GetMapping("/currency/{currencyName}/{datefrom}/{dateto}")
     ResponseEntity<currency> currency(@PathVariable String currencyName, @PathVariable Date datefrom, @PathVariable Date dateto)
     {
-        return ResponseEntity.ok(new currency());
-//        double average = myService.getCurrency(currencyName,datefrom,dateto);
-//        currency NEWCURRENCY = new currency(0,currencyName,dateto,datefrom,average,new Date());
-//        return ResponseEntity.ok(myService.addCurrency(NEWCURRENCY));
+        double average = myService.getCurrency(currencyName,datefrom,dateto);
+        currency NEWCURRENCY = new currency(0,currencyName,dateto,datefrom,average,new Date());
+        return ResponseEntity.ok(myService.addCurrency(NEWCURRENCY));
     }
 
 }
