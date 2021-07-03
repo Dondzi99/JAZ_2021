78f8a204da0afc67ccc9b115135d23098d7cf229
Paweł Dondziak <56679544+Dondzi99@users.noreply.github.com>
2021-07-03 18:07:16 +0200

    ad

--- jaz-s18946-nbp/src/main/java/pj/s18946/demo/controller.java
+++ jaz-s18946-nbp/src/main/java/pj/s18946/demo/controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 // główny kontroler aplikacji
@@ -22,12 +23,15 @@ public class controller {
     // endpoint pozwalający na otrzymanie informacji o sredniej watrosci waluty
     // z wczesniejszym przekazaniem przez uzytkownika nazwy waluty, daty od, daty do(podanych w dniach)
     // w odpowiedzi otrzymujemy obikt json typu currency
+    String pattern = "yyyy-MM-dd";
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
     @GetMapping("/currency/{currencyName}/{datefrom}/{dateto}")
     ResponseEntity<currency> currency(@PathVariable String currencyName, @PathVariable Date datefrom, @PathVariable Date dateto)
     {
-        double average = myService.getCurrency(currencyName,datefrom,dateto);
-        currency NEWCURRENCY = new currency(0,currencyName,dateto,datefrom,average,new Date());
-        return ResponseEntity.ok(myService.addCurrency(NEWCURRENCY));
+        return ResponseEntity.ok(new currency());
+//        double average = myService.getCurrency(currencyName,datefrom,dateto);
+//        currency NEWCURRENCY = new currency(0,currencyName,dateto,datefrom,average,new Date());
+//        return ResponseEntity.ok(myService.addCurrency(NEWCURRENCY));
     }
 
 }
